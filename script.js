76f35e4b384fb47a83658f37b62b5a3e2889bf21
sagar joshi <sagarjo637@gmail.com>
2026-03-27 10:42:36 +0545

    added event on quote btn

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const getQuote = document.getElementById("quote");
 const getAuthor = document.getElementById("author");
 const quoteBtn = document.getElementById("btn");
+
+//adding functionality on generate quote btn
+quoteBtn.addEventListener("click", async () => {
+    
+})
