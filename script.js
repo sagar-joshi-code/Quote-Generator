10846e516c13ca9fa390280c88fd61d69c93fb15
sagar joshi <sagarjo637@gmail.com>
2026-03-27 13:13:41 +0545

    fetching api and completed project

--- script.js
+++ script.js
@@ -4,5 +4,23 @@ const quoteBtn = document.getElementById("btn");
 
 //adding functionality on generate quote btn
 quoteBtn.addEventListener("click", async () => {
-    
-})
+  getQuote.textContent = "Loading....";
+  getAuthor.textContent = "";
+
+  //added  Add try-catch block for API call
+  try {
+    const res = await fetch("https://dummyjson.com/quotes/random");
+    const data = await res.json();
+
+    getQuote.textContent = data.quote;
+    quoteBtn.disabled = true;
+    setTimeout(() => {
+      getAuthor.textContent = `-${data.author}`;
+      quoteBtn.disabled = false;
+    }, 2000);
+  } catch (error) {
+    getQuote.textContent = "Failed to load a quote";
+    getAuthor.textContent = "";
+    quoteBtn.disabled = false;
+  }
+});
